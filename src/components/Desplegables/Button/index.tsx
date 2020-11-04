e40a0baf8c7628c7f41eed187df6d90971e6cce5
samRgarcia <samuelrivas18@gmail.com>
2020-11-03 18:08:41 -0600

    avance maquetacion

---
+++ src/components/Desplegables/Button/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './styleButton.css';
+
+const Button = ()=>{
+    return(<div>
+        <ul className="nav-menu">
+            <li><a className='perfil'>MS</a>
+            <ul>
+                <li><a href="#" >Salir</a></li>
+
+            </ul>
+            </li>
+
+
+        </ul>
+    </div>)
+}
+
+export default Button;
--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
-import './ToolBar.css'
+import './ToolBar.css';
+import ButtonOp from '../Desplegables/Button';
 
 const Toolbar=(props:any)=>{
     return(<header className='toolbar'>
         <nav className='toolbar_navigation'>
             <div className='toolbar_toggle-button'><DrawerToggleButton click={props.drawerClickHeandler}/></div>
-            <div className='toolbar_logo' ><a href='#'>the logo</a></div>
+            <div className='toolbar_logo' ><a href='#'>The logo</a></div>
             <div className='spacer'/>
             <div className='toolbar_navigation-item'>
                 <ul>
-                  <li><a href="#">Produc</a></li>
+                  <li><a href="#">Home</a></li>
                     <li><a href="#">User</a></li>
                 </ul>
             </div>
+            <ButtonOp/>
         </nav>
     </header>)
 }
